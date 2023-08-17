ff18b2489c9213ba6aeb8a9705bd4f2c9eeaab5a
Max Frohmüller <m.frohmue@web.de>
2023-08-17 20:53:43 +0200

    initial commit

---
+++ tests/all_test.py
@@ -0,0 +1,17 @@
+
+# one test to rule them all (/both)
+
+############################################# IMPORTS #################################################
+
+import unittest
+
+# comment out tests you dont want to run here
+from mainfraime_test            import Mainfraime_test
+from util_test                  import UTIL_test      
+
+
+
+
+############################################### MAIN ###################################################
+
+unittest.main()
